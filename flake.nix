c4c87ed7131059d59025521814aa9da37a73d427
frectonz <fraol0912@gmail.com>
2024-06-28 17:05:17 +0300

    runnable via nix

--- flake.nix
+++ flake.nix
@@ -7,13 +7,27 @@
   outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
+
+      app = pkgs.beamPackages.buildMix rec {
+        name = "hazarda_en_elixir";
+        version = "0.0.1";
+        src = ./.;
+
+        postBuild = ''
+          mix escript.build --no-deps-check
+        '';
+
+        installPhase = "install -Dt $out/bin ${name}";
+      };
     in
-    with pkgs; {
-      devShells.default = mkShell {
-        buildInputs = [ elixir elixir-ls erlang ];
+    {
+      packages.default = app;
+
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ elixir elixir-ls erlang ];
       };
 
-      formatter = nixpkgs-fmt;
+      formatter = pkgs.nixpkgs-fmt;
     }
   );
 }
